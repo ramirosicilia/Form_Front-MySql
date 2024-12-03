76fe5b0ac379a4e15010f53ade3f4be898512621
ramiroSicilia <moto2.belgrano@gmail.com>
2024-12-02 21:06:40 -0300

    Eliminar archivos rastreados previamente que están en .gitignore

---
+++ app.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import cors from 'cors';
+import { obtenerFormulario, obtenerINFO, obtenerVariablesEntorno, obtenerLogin, obtenerEliminacion, obtenerContraseñas } from './controllers/ControladoresFront.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+const __filName = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filName);
+
+
+
+app.use(express.static(path.join(__dirname, './frontend')));  // Ruta para archivos estáticos
+
+app.set('view engine','ejs')
+app.set('views', path.join(__dirname, './frontend/views'));  // Aquí s la clave para el error
+
+const port = process.env.PUERTO;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/obtener-info", obtenerINFO);
+app.get('/', obtenerFormulario);
+app.get("/login", obtenerLogin);
+app.get("/validar-contrasenas", obtenerContraseñas);
+app.get("/variables", obtenerVariablesEntorno);
+app.get("/eliminar-user", obtenerEliminacion);
+
+app.listen(port, () => {
+    console.log(`Servidor escuchando en el puerto ${port}`);
+});
---
+++ controllers/ControladoresFront.js
@@ -0,0 +1,77 @@
+import dotenv from "dotenv" 
+
+
+dotenv.config() 
+
+ const urlBack=process.env.URL_BACKEND 
+
+ const urlFront=process.env.URL_FRONT 
+ console.log(`se esucha el back ${urlBack}`) 
+ console.log(urlFront)
+
+ export const obtenerFormulario=async(req,res)=>{ 
+
+    try{ 
+        res.render('index',{url:urlFront ,urlBack:urlBack})
+    
+     } 
+     catch(err){
+        console.log('a ocurrido un error',err)
+     }
+    
+
+
+ } 
+
+ export const obtenerVariablesEntorno=async(req,res)=>{ 
+
+ try{ 
+    res.json({urlBack:urlBack})
+
+ } 
+ catch(err){
+    console.log('a ocurrido un error',err)
+ }
+
+
+ } 
+
+ export const obtenerINFO=async(req,res)=>{ 
+
+   const response= await fetch(`${urlBack}/obtener-info`)  
+   let data=await response.json()
+ 
+   res.render('pages',{users:data.usuario})
+
+ }  
+
+
+ export const obtenerLogin=async(req,res)=>{ 
+
+   
+    
+    res.render('login',{urlFront:urlFront,urlBack:urlBack})
+ 
+  } 
+
+  export const obtenerContraseñas=async(req,res)=>{ 
+
+   
+    
+    res.render('contrasena',{Back:urlBack})
+ 
+  } 
+
+  export const obtenerEliminacion=async(req,res)=>{ 
+
+   
+    
+    res.render('eliminar',{urlBack:urlBack})
+ 
+  } 
+
+
+
+
+
+
